cb5a934f4c14c52d4cd7bbe9e38ea62abcdb18e3
Muffinswap <79991815+Muffinswap@users.noreply.github.com>
2021-03-14 20:58:06 +0700

    Init

--- src/constants/eggPriceContracts.ts
+++ src/constants/eggPriceContracts.ts
@@ -1,7 +1,7 @@
 const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
   cakeAddress: '0xebc46c9898f675eefef43ec410dd91edbf0006c1',
   busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
-  lpAddress: '0x19e7cbecdd23a16dfa5573df54d98f7caae03019'
+  lpAddress: '0x03b77404b582667ab7e7aae1116229e9e2038f31'
 }
 
 export default priceContracts
